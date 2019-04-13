d8b979f93f949ac383711d28cca05def881eb8a9
Lucas Alexandre <lucas.alexandre.df@gmail.com>
2019-04-13 15:29:17 -0300

    [#44] Criado estrutura de pasta.
    Signed-off-by: Lucas Alexandre <lucas.alexandre.df@gmail.com>
    Co-authored-by: MatheusEstanislau <matheus.estanislau7@gmail.com>

---
+++ src/Components/TelaLogin/TelaLogin.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './TelaLogin.css'
+
+class TelaLogin extends Component {
+  render() {
+    return (
+        <h1></h1>
+    );   
+  }
+}
+
+export default TelaLogin;
