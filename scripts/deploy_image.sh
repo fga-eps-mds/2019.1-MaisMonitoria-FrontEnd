5e4d83e198301b3f2709c20393692dd4f92a1971
matheus-rn <matheus.1nascimento58@gmail.com>
2019-04-28 17:05:39 -0300

    [REFACTOR] Adicionados arquivos de deploy das imagens no dockerhub

--- scripts/deploy_image.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -ev
-    
-echo "Deployment init"
-
-echo "$DOCKERPASSWORD" | docker login -u "$DOCKERUSERNAME" --password-stdin
-docker tag maismonitoria/frontend maismonitoria/frontend:latest
-docker push maismonitoria/frontend:latest  
