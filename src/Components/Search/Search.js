f9dd5aa669fb6bd5bc163a560a396d8c122957b2
MoacirMSJ <moacirmascarenha86@gmail.com>
2019-05-17 22:04:24 -0300

    [US06] Inserido cards no sistema de pesquisa.
    Signed-off-by: MoacirMSJ <moacirmascarenha86@gmail.com>
    Co-authored-by: lucasA27 <lucas.alexandre.df@gmail.com>

--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -1,22 +1,51 @@
 import React, { Component } from 'react';
-import { Grid} from '@material-ui/core' ;
+import { Grid,TextField } from '@material-ui/core' ;
 import AppBar from './AppBarSearch';
 import Paper from './PaperSearch';
+import firebase from 'firebase';
+import axios from 'axios';
+import Card from '../Feed/Card';
 
 
 class Search extends Component {
+    state =  {expanded: false,data : [],pesquisa:''}
     
+    handleChange(e){
+        const title = e.target.value;
+        var token = {
+            search: title,
+        };
+        firebase.auth().onAuthStateChanged(user =>{
+            this.setState({isSignedIn: !!user});
+            if(user){
+                firebase.auth().currentUser.getIdToken().then(function(idToken){
+                    token["access_token"] = idToken;
+                });
+                axios.post(process.env.REACT_APP_GATEWAY+"/search_tutoring/", token)
+                    .then(res => {
+                        const person = res.data
+                        this.setState({data:person})
+                        console.log(this.state.data);
+                    });
+            }
+          });
+    }
+
   render() {
-      
+    console.log("renderizou");
     return (
         <div style={{overflowX:'hidden'}} >
             <Grid container  justify="center" alignItems="stretch">
-                <AppBar/>
+                <input value={this.props.title} onChange={this.handleChange.bind(this)} />
             </Grid>    
             <Grid container  justify="center" direction="column" alignItems="center" spacing="16" style={{ padding: 80 }}>
-                <Grid item >
-                    <Paper/>
-                </Grid>
+            {this.state.data.map(function(item, i){
+                    return (
+                        <Grid item key={i} lg={12} sm={12} container >
+                            <Card name_monitoring={item.name} matter={item.subject} deion={item.description}/>
+                        </Grid>
+                    );
+                })}
             </Grid>
         </div>
     );   
