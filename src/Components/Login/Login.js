4bd12d9c1837e818aad45e4dc0787d123a508d1a
RCristyan <rcristyan9@gmail.com>
2019-05-25 10:09:42 -0300

    [BUG] Adicionado spinner no botão de login e ajustes na identação do código.

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -4,7 +4,9 @@ import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import lightBlue from '@material-ui/core/colors/lightBlue';
 import  {Link}  from 'react-router-dom';
 import firebase from 'firebase';
+import { withRouter } from 'react-router-dom';
 
+import Spinner from '../Loader/Spinner';
 import CustomizedSnackbars from '../SimpleModal/Snackbars';
 import logo from '../../Assets/img/Logo.png';
 import './Login.css';
@@ -30,6 +32,7 @@ class Login extends Component {
     isAuthenticated: false,
     error: "",
     showError: false,
+    isLoading: false
   };
   
 
@@ -41,12 +44,16 @@ class Login extends Component {
       this.setState({ showError: true});
       e.preventDefault();
     }else{
+      this.setState({ isLoading: true });
       await firebase.auth().signInWithEmailAndPassword(email, password).then((user)=>{
-        this.setState({isAuthenticated: true});
+        this.setState({ isAuthenticated: true });
+        const route = this.state.isAuthenticated?"/":"/Login"
+        this.props.history.push(route);
       }).catch((except)=>{
         this.setState({ error: "Email ou Senha inválidos." });
-        this.setState({ showError: true});
+        this.setState({ showError: true });
       });
+      this.setState({ isLoading: false });
     }
   }
 
@@ -54,58 +61,62 @@ class Login extends Component {
     return (
       <div className="LoginBackground" style={{overflowY:'auto',overflowX:'hidden'}}>
         <Grid container alignContent="center" justify="center" direction="column" alignItems="center">
-          
-          <img src={logo} alt="Logo" />
-          <Grid item >
-            <TextField
-              error = {this.state.error}
-              id="emailTextField"
-              label="Email"
-              margin="normal"
-              required= "true"
-              value={this.state.email}
-              onChange={(event)=>this.setState({
-              email: event.target.value,
-              })}
-              />
-          </Grid>
-          <Grid item >
-            <TextField
-              error = {this.state.error}
-              id="senhaTextField"
-              label="Senha"
-              margin="normal"
-              type="password"
-              required= "true"
-              value={this.state.password}
-              onChange={(event)=>this.setState({
-              password: event.target.value,
-              })}
-              />
-          </Grid>
-          <Grid container alignContent="center" justify="center" direction="row" spacing={40} alignItems="center">
+          <div style={{ padding: 80}}>  
+            <Grid container alignContent="center" justify="center" direction="column" alignItems="center" spacing={24}>
+              <img src={logo} alt="Logo" />
+              <Grid item >
+                <TextField
+                  error = {this.state.error}
+                  id="emailTextField"
+                  label="Email"
+                  margin="normal"
+                  required= "true"
+                  value={this.state.email}
+                  onChange={(event)=>this.setState({
+                  email: event.target.value,
+                  })}
+                  />
+              </Grid>
+              <Grid item >
+                <TextField
+                  error = {this.state.error}
+                  id="senhaTextField"
+                  label="Senha"
+                  margin="normal"
+                  type="password"
+                  required= "true"
+                  value={this.state.password}
+                  onChange={(event)=>this.setState({
+                  password: event.target.value,
+                  })}
+                  />
+              </Grid>
               {this.state.showError? <CustomizedSnackbars error={this.state.error}/>:null}
-          </Grid>
+            </Grid>
+            { this.state.isLoading ? <Spinner />: 
+              <Grid  container alignContent="center" justify="center" direction="column" spacing={24} alignItems="center" style={{marginTop: 25}}>
+                <Grid item >
+                  <MuiThemeProvider theme={theme}>
+                    <Button component={Link} to={this.state.isAuthenticated?"/Feed":"/"} variant="outlined" color="primary" onClick={this.login}>
+                        Login
+                    </Button>
+                  </MuiThemeProvider>
+                </Grid>  
+                <Grid item>
+                  <MuiThemeProvider theme={theme}>
+                    <Button component={Link} to="/SignUp" variant="outlined" color="primary">
+                      Registrar
+                    </Button>
+                  </MuiThemeProvider>
+                </Grid>  
+                <Link to="/ForgotPassword" style={{color:'#bdbdbd'}} >
+                  Esqueceu sua senha ?
+                </Link>
+              </Grid>
+            }
+          </div>
         </Grid>
-        <Grid  container alignContent="center" justify="center" direction="column" spacing={24} alignItems="center" style={{marginTop: 25}}>
-          <Grid item >
-            <MuiThemeProvider theme={theme}>
-              <Button component={Link} to={this.state.isAuthenticated?"/Feed":"/"} variant="outlined" color="primary" onClick={this.login}>
-                  Login
-              </Button>
-            </MuiThemeProvider>
-          </Grid>  
-          <Grid item>
-            <MuiThemeProvider theme={theme}>
-              <Button component={Link} to="/SignUp" variant="outlined" color="primary">
-                Registrar
-              </Button>
-              </MuiThemeProvider>
-          </Grid>  
-              <Link to="/ForgotPassword" style={{color:'#bdbdbd'}} >Esqueceu sua senha ?</Link>
-        </Grid>
-          
-        </div>
+      </div>
     );
   }
 }
