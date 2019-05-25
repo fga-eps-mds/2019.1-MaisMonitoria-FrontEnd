7b38b55b96ea36a1a75de7ba515372363bdaf368
Matheus Estanislau <matheus.estanislau7@gmail.com>
2019-05-24 23:29:00 -0300

    [US19] Atualizados testes.
    Signed-off-by: Matheus Estanislau <matheus.estanislau7@gmail.com>
    Co-authored-by: RCristyan <rcristyan9@gmail.com>

--- src/Components/__tests__/AppBarWithBack.test.js
+++ src/Components/__tests__/AppBarWithBack.test.js
@@ -1,14 +1,25 @@
 import React from 'react';
-import AppBarWithBack from '../Feed/AppBarWithBack';
-import renderer from 'react-test-renderer';
+import AppBar from '../Feed/AppBarWithBack';
 import toJson from 'enzyme-to-json';
+import renderer from 'react-test-renderer';
 import { MemoryRouter as Router } from 'react-router-dom';
+import firebase from 'firebase';
 
-const appbarwithback = jest.fn();
+firebase.initializeApp({
+  apiKey: '...',
+  authDomain: '...',
+  databaseURL: '...',
+  projectId: '...',
+  storageBucket: '...',
+  messagingSenderId: '...',
+});
 
-  it('Test if AppBarrWithBack renders correctly', () =>{
+const appBar = jest.fn();
+
+
+  it('Test if Course renders correctly', () =>{
     const tree = renderer.create(
-      <Router><AppBarWithBack profile={appbarwithback}/></Router>
+      <Router><AppBar appBar={appBar}/></Router>
       )
       expect(toJson(tree)).toMatchSnapshot()
   });
